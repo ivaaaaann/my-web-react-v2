958ca77cca9ebdfdca93fe29bc78cd1589377ef7
임동현 <79711744+ldh3907@users.noreply.github.com>
2021-12-14 16:49:09 +0900

    ADD : 기본세팅
    라우터 세팅, 여러 라이브러리 세팅

---
+++ src/Theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  main_color: "182153",
+  subBg: "f1f1f1",
+  mainBg: "#ffffff",
+};
---
+++ src/styled.d.ts
@@ -0,0 +1,9 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    main_color: "182153";
+    subBg: "f1f1f1";
+    mainBg: "#ffffff";
+  }
+}
