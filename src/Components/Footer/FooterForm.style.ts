c2ed2b61f6a00a347014279f97e6598c43b84542
ldh3907 <ldh165163@gmail.com>
2022-01-30 20:21:15 +0900

    UPDATE : 푸터

--- src/Components/Footer/FooterForm.style.ts
+++ src/Components/Footer/FooterForm.style.ts
@@ -14,6 +14,8 @@ export const FooterWrap = styled.div`
   margin: 0px auto;
   padding: 60px 0px;
   box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const FooterTitle = styled.h1`
@@ -79,5 +81,60 @@ export const FooterUl = styled.ul`
       text-decoration: underline;
       animation: ${liAnimation} 2s linear infinite;
     }
+  }
+`;
+
+const footerItemAnimation = keyframes`
+    0% {
+        left: 0px;
+      }
+    100% {
+      left: -640px;
+    }
+`;
+
+export const FooterSlideWrap = styled.div`
+  margin: 0px auto;
+  margin-top: 60px;
+  width: 390px;
+  height: 46px;
+  display: flex;
+  overflow: hidden;
+  align-items: center;
+  position: relative;
+`;
+
+export const FooterSlideItemWrap = styled.div`
+  display: flex;
+  height: 46px;
+  align-items: center;
+  position: absolute;
+  animation: ${footerItemAnimation} 5s linear infinite;
+`;
+
+export const FooterSlideItem = styled.div`
+  width: 46px;
+  height: 46px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin: 0px 30px;
+  &:first-child {
+    margin-left: 0px;
+  }
+  &:last-child {
+    margin-right: 0px;
+  }
+`;
+
+export const FooterCopyLightText = styled.h1`
+  font-size: 14px;
+  margin-top: auto;
+  text-align: center;
+  color: white;
+
+  strong {
+    color: grey;
   }
 `;
--- src/Components/Header/HeaderForm.style.ts
+++ src/Components/Header/HeaderForm.style.ts
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const HeaderContainer = styled(motion.div)`
   width: 100%;
